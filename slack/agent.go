a0b335cd9ee3b6bf61bb6f8c6a4d19897341efb4
kevin <wanjunfeng@gmail.com>
2021-10-26 22:17:51 +0800

    support slack

--- slack/agent.go
+++ slack/agent.go
@@ -1,19 +1,60 @@
 package slack
 
-import "github.com/slack-go/slack"
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"net/http"
 
-func Send(token, channel, message string) error {
-	api := slack.New(token)
-	attachment := slack.Attachment{
-		Pretext: "Pretext",
-		Text:    "Hello from GolangDocs!",
+	"github.com/tal-tech/go-zero/core/jsonx"
+	"github.com/tal-tech/go-zero/core/logx"
+)
+
+const slackPostMessageUrl = "https://slack.com/api/chat.postMessage"
+
+type (
+	Request struct {
+		Channel string `json:"channel"`
+		Text    string `json:"text"`
 	}
 
-	_, _, err := api.PostMessage(
-		channel,
-		slack.MsgOptionText("This is the main message", false),
-		slack.MsgOptionAttachments(attachment),
-		slack.MsgOptionAsUser(true),
-	)
-	return err
+	Response struct {
+		OK    bool   `json:"ok"`
+		Error string `json:",optional"`
+	}
+)
+
+func Send(token, channel, message string) error {
+	slackMsg := Request{
+		Channel: channel,
+		Text:    message,
+	}
+	var buf bytes.Buffer
+	encoder := json.NewEncoder(&buf)
+	if err := encoder.Encode(slackMsg); err != nil {
+		return err
+	}
+
+	r, err := http.NewRequest(http.MethodPost, slackPostMessageUrl, &buf)
+	if err != nil {
+		return err
+	}
+
+	r.Header.Set("Authorization", "Bearer "+token)
+	resp, err := new(http.Client).Do(r)
+	if err != nil {
+		return err
+	}
+
+	var rsp Response
+	if err := jsonx.UnmarshalFromReader(resp.Body, &rsp); err != nil {
+		return err
+	}
+
+	if !rsp.OK {
+		return errors.New(rsp.Error)
+	}
+
+	logx.Info("sent")
+	return nil
 }
--- stargazers.go
+++ stargazers.go
@@ -6,6 +6,7 @@ import (
 
 	"stargazers/feishu"
 	"stargazers/gh"
+	"stargazers/slack"
 
 	"github.com/tal-tech/go-zero/core/conf"
 	"github.com/tal-tech/go-zero/core/logx"
@@ -13,11 +14,44 @@ import (
 
 var configFile = flag.String("f", "config.yaml", "the config file")
 
-type Config struct {
-	Token    string        `json:"token"`
-	Repo     string        `json:"repo"`
-	Interval time.Duration `json:"interval,default=1m"`
-	Feishu   feishu.Feishu `json:"feishu,optional"`
+type (
+	Config struct {
+		Token    string         `json:"token"`
+		Repo     string         `json:"repo"`
+		Interval time.Duration  `json:"interval,default=1m"`
+		Feishu   *feishu.Feishu `json:"feishu,optional"`
+		Slack    *slack.Slack   `json:"slack,optional"`
+	}
+
+	SendFunc func(message string) error
+)
+
+func getSenders(c Config) []SendFunc {
+	var senders []SendFunc
+
+	if c.Feishu != nil {
+		senders = append(senders, func(message string) error {
+			return feishu.Send(
+				c.Feishu.AppId,
+				c.Feishu.AppSecret,
+				c.Feishu.Receiver,
+				c.Feishu.ReceiverEmail,
+				message,
+			)
+		})
+	}
+
+	if c.Slack != nil {
+		senders = append(senders, func(message string) error {
+			return slack.Send(
+				c.Slack.Token,
+				c.Slack.Channel,
+				message,
+			)
+		})
+	}
+
+	return senders
 }
 
 func main() {
@@ -25,9 +59,16 @@ func main() {
 
 	var c Config
 	conf.MustLoad(*configFile, &c)
+	senders := getSenders(c)
 
 	mon := gh.NewMonitor(c.Repo, c.Token, c.Interval, func(text string) error {
-		return feishu.Send(c.Feishu.AppId, c.Feishu.AppSecret, c.Feishu.Receiver, c.Feishu.ReceiverEmail, text)
+		for _, sender := range senders {
+			if err := sender(text); err != nil {
+				return err
+			}
+		}
+
+		return nil
 	})
 	logx.Must(mon.Start())
 }
